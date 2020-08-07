2ba59caddeb088d6f54692f0407cfd7306085ad1
ternovykh <ternovykhirina@yandex.ru>
2020-08-07 14:46:15 +0300

    добавлены шрифты

--- webpack.config.js
+++ webpack.config.js
@@ -62,17 +62,17 @@ module.exports = {
                 test: /\.pug$/,
                 loader: 'pug-loader'
             },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+               'file-loader',
+               ],
+            },
 
         ]
     },
     plugins: [
         new MinCssPlugin(),
-        new HtmlWebpackPlugin({
-            inject: false,
-            hash: true,
-            template: './src/index.html',
-            filename: 'index.html'
-        }),
         ...PAGES.map(page => new HtmlWebpackPlugin({
             template: `${PAGES_DIR}/${page}`,
             filename: `./${page.replace(/\.pug/,'.html')}`
